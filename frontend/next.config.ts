6fda2c412603bbc67c717d8d02b87c8ffad7c480
Henrique Jardim <contato@agenciacodigo.com>
2026-01-28 12:59:39 -0300

    Feat: Configurando Rewrites para corrigir HTTPS na Vercel

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -10,11 +10,23 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'dsr.v4jasson.com.br', 
-        port: '',
+        hostname: 'dsr.v4jasson.com.br',
         pathname: '/media/**',
       }
     ],
+  },
+  // O SEGredo ESTÁ AQUI EMBAIXO:
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://31.97.242.139:8000/api/:path*',
+      },
+      {
+        source: '/media/:path*',
+        destination: 'http://31.97.242.139:8000/media/:path*',
+      },
+    ];
   },
 };
 
