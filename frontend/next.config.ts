3a574e07e19b985b854c7bc8d050fd82eab56bb8
Henrique Jardim <contato@agenciacodigo.com>
2026-01-28 11:35:54 -0300

    Fix: Correção do next.config.mjs para imagens da VPS

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '31.97.242.139',
+        port: '8000',
+        pathname: '/media/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dsr.v4jasson.com.br', 
+        port: '',
+        pathname: '/media/**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
